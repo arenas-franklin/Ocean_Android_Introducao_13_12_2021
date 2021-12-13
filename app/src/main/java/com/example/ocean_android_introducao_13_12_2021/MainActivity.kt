9702c7e1799a4e1a426097b3ad9000b71e1c8f5f
Franklin Angelo <angelod1@hotmail.com>
2021-12-13 14:38:57 -0300

    Atualização da textView

--- app/src/main/java/com/example/ocean_android_introducao_13_12_2021/MainActivity.kt
+++ app/src/main/java/com/example/ocean_android_introducao_13_12_2021/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.ocean_android_introducao_13_12_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Acessar o elemento que está no layout XML
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val etNome = findViewById<EditText>(R.id.etNome)
+
+
+
+        // Declaramos um ouvinte para evento de clique no  btEnviar
+        // Assim que um clique no btEnviar ocorrer, o código  dentro
+        // das chaves {} será executado
+        btEnviar.setOnClickListener{
+            val nome = etNome.text
+
+            // Alteramos o texto do tvResultado para uma string que quisermos
+            tvResultado.text = nome
+        }
+
     }
 }
